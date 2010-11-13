33b304c87ea1bca09e55c8bbe2e3f91ba288478e
Spike Grobstein <spikegrobstein@mac.com>
2010-11-13 13:34:54 -0800

    rewrote the router class as RouterCore

--- router.js
+++ router.js
@@ -1,37 +1,78 @@
+function RouterCore(route_callback) {
+	if (route_callback) {
+		this.route = route_callback;
+	}
+}
+
+/*
+**	start up the router. set up all <a> tags to
+**	be powered by this object if their href attribute
+**	starts with a #
+*/
+RouterCore.prototype.boot = function() {
+	var router = this;
+	
+	// any link that gets clicked should do this...
+	$('a').live('click', function() {
+		// if a non-anchor link is clicked, don't handle it.
+		if (!$(this).attr('href').match(/^#/)) {
+			return true;
+		}
+
+		// handle the click
+		router.route(router.strip_hash($(this).attr('href')));
+	});
+
+	// if a section was requested as part of the page-load, let's get it.
+	router.route(this.strip_hash(window.location.hash));
+}
+
+/*
+**	this gets overridden by you to handle routing
+*/
+RouterCore.prototype.route = function(request) {
+	throw "You need to override the Request.route() function, dude.";
+}
+
+/*
+**	strips the leading # off the request
+*/
+RouterCore.prototype.strip_hash = function(request) {
+	var m = request.match(/^#(.*$)/);
+	
+	if (m) {
+		return m[1];
+	}
+	
+	return request;
+}
+/*
+Router.prototype.parse = function(request) {
+	if (!request) {
+		// if href is not specified, pull it from the page URL
+		request= window.location.hash;
+	}
+
+	var m = request.match(/^\/?#\/?(.+?)$/);
+
+	if (!m) {
+		return '';	
+	}
+	
+	return m[1];
+}
+
+
 var Router = {
 	boot: function() {
-		// any link that gets clicked shoudl do this...
-		$('a').live('click', function() {
-			// if a non-anchor link is clicked, don't handle it.
-			if (!$(this).attr('href').match(/^#/)) {
-				return true;
-			}
 
-			// handle the click
-			Router.route(Router.parse($(this).attr('href')));
-		});
-
-		// if a section was requested as part of the page-load, let's get it.
-		Router.route(Router.parse());
 	},
 	
 	route: function(request) {
-		throw "You need to override the Request.route() function, dude.";
 	},
 
 	parse: function(href) {
-		if (!href) {
-			// if href is not specified, pull it from the page URL
-			href= window.location.hash;
-		}
 
-		var m = href.match(/^\/?#\/?(.+?)$/);
-
-		if (!m) {
-			return '';	
-		}
-		
-		return m[1];
 	
 	},
 	
@@ -73,4 +114,4 @@ var Router = {
 	}
 }
 
-
+*/
