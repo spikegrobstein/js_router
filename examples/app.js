c3415d436a1c96f17646d5ebb263fb2544701944
Spike Grobstein <spikegrobstein@mac.com>
2010-11-13 23:46:55 -0800

    moved code into examples directory

---
+++ examples/app.js
@@ -0,0 +1,51 @@
+var router = new MappingRouter({
+	root: {
+		index: function(params) {
+			console.log(params);
+		}
+	},
+	
+	people: {
+		index: function(params) {
+			console.log(params);
+		},
+		show: function(params) {
+			console.log(params);
+		},
+		'new': function(params) {
+			console.log(params);
+		},
+		edit: function(params) {
+			console.log(params);
+		},
+	},
+	
+	events: {
+		index: function(params) {
+			console.log(params);
+		},
+		show: function(params) {
+			console.log(params);
+		},
+		'new': function(params) {
+			console.log(params);
+		},
+		edit: function(params) {
+			console.log(params);
+		},
+	},
+	
+	home: {
+		index: function(params) {
+			console.log(params);
+		}
+	},
+	
+	about: {
+		index: function(params) {
+			console.log(params);
+		}
+	}
+});
+
+router.boot();
